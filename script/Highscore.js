8f64118049f8887ae42ea7ed97b9740da0681b59
sanjeevs9 <sanjeev.19kr@gmail.com>
2024-05-13 23:12:28 +0530

    some fix

--- script/Highscore.js
+++ script/Highscore.js
@@ -1,24 +1,24 @@
 
 
-// document.addEventListener("DOMContentLoaded",()=>{
-//     axios.get(`${network}/score/high`).then(res=>{
-//         console.log(res.data.value)
-//         let count=1;
-//         let placeholder=document.getElementById("output");
-//         let out=""
-//         res.data.value.forEach(element => {
-//             out +=`
-//                 <tr>
-//                     <td>#${count}</td>
-//                     <td>${element.id.name}</td>
-//                     <td>${element.wpm} Wpm</td>
-//                 </tr>
-//             `
-//             count++;
-//         });
-//         placeholder.innerHTML=out
-//     }).catch(err=>{
-//         console.log(err)
-//     })
+document.addEventListener("DOMContentLoaded",()=>{
+    axios.get(`${network}/score/high`).then(res=>{
+        console.log(res.data.value)
+        let count=1;
+        let placeholder=document.getElementById("output");
+        let out=""
+        res.data.value.forEach(element => {
+            out +=`
+                <tr>
+                    <td>#${count}</td>
+                    <td>${element.id.name}</td>
+                    <td>${element.wpm} Wpm</td>
+                </tr>
+            `
+            count++;
+        });
+        placeholder.innerHTML=out
+    }).catch(err=>{
+        console.log(err)
+    })
 
-// })
+})
